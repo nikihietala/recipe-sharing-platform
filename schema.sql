8c613c2797428eeffc03060b1db76932d5b69478
Niki Hietala <niki.hietala@gmail.com>
2023-09-27 19:31:09 +0300

    added more sql tables

--- schema.sql
+++ schema.sql
@@ -3,4 +3,32 @@ CREATE TABLE users (
     name TEXT UNIQUE,
     password TEXT,
     role INTEGER
-);
+)
+
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
+    recipe_name TEXT,
+    price INTEGER,
+    rating FLOAT,
+    poster_id INTEGER REFERENCES users(id),
+)
+
+CREATE TABLE ingredients (
+    id SERIAL PRIMARY KEY,
+    ingredient_name TEXT,
+    recipe_id INTEGER REFERENCES recipes(id)
+)
+
+CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    recipe_id INTEGER REFERENCES recipes(id)
+)
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    comment TEXT,
+    user_id INTEGER REFERENCES users(id),
+    recipe_id INTEGER REFERENCES recipes(id),
+    comment_date DATE
+)
