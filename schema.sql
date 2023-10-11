99a5aaf67c85418213abe060eed506757a72605c
Niki Hietala <niki.hietala@gmail.com>
2023-10-11 16:26:09 +0300

    update sql tables

--- schema.sql
+++ schema.sql
@@ -3,32 +3,32 @@ CREATE TABLE users (
     name TEXT UNIQUE,
     password TEXT,
     role INTEGER
-)
+);
 
 CREATE TABLE recipes (
     id SERIAL PRIMARY KEY,
-    recipe_name TEXT,
-    price INTEGER,
-    rating FLOAT,
-    poster_id INTEGER REFERENCES users(id),
-)
+    description TEXT,
+    price DECIMAL,
+    rating INTEGER,
+    poster_name TEXT REFERENCES users(name)
+);
 
 CREATE TABLE ingredients (
     id SERIAL PRIMARY KEY,
     ingredient_name TEXT,
-    recipe_id INTEGER REFERENCES recipes(id)
-)
+    recipe_id INTEGER REFERENCES recipes
+);
 
 CREATE TABLE favorites (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    recipe_id INTEGER REFERENCES recipes(id)
-)
+    user_id INTEGER REFERENCES users,
+    recipe_id INTEGER REFERENCES recipes
+);
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     comment TEXT,
-    user_id INTEGER REFERENCES users(id),
-    recipe_id INTEGER REFERENCES recipes(id),
+    user_id INTEGER REFERENCES users,
+    recipe_id INTEGER REFERENCES recipes
     comment_date DATE
-)
+);
